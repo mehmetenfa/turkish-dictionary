48c89e5fb1115a286c1a707331e20617e422da4c
qxan <mehmet.enfa@gmail.com>
2023-02-15 20:38:18 +0300

    native stack

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-export default function App() {
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>hey! whats up NIGGA!</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
     </View>
   );
 }
+const Stack = createNativeStackNavigator()
 
-const styles = StyleSheet.create({
-  
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>hey! whats up NIGGA!</Text>
+      </View>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({});
